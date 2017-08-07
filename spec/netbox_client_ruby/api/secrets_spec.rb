9427b8320ca5522881aeccafcf59d827bb1d69ea
Christian Mäder <cma@nine.ch>
2017-08-07 15:00:51 +0200

    ✅ Adds new specs for the 'secrets' feature

--- spec/netbox_client_ruby/api/secrets_spec.rb
+++ spec/netbox_client_ruby/api/secrets_spec.rb
@@ -6,7 +6,7 @@ module NetboxClientRuby
 
       {
         secret_roles: SecretRoles,
-        secrets: Secrets
+        secrets: NetboxClientRuby::Secrets::Secrets
       }.each do |method, klass|
         describe ".#{method}" do
           subject { NetboxClientRuby::Secrets.public_send(method) }
@@ -27,6 +27,32 @@ module NetboxClientRuby
           end
         end
       end
+
+      {
+        secret: Secret,
+        secret_role: SecretRole
+      }.each do |method, expected_class|
+        describe ".#{method}" do
+          let(:id) { 1 }
+          subject { NetboxClientRuby::Secrets.public_send(method, id) }
+
+          context 'is of the expected type' do
+            it { is_expected.to be_a expected_class }
+          end
+
+          context 'it is a new instance each time' do
+            it do
+              is_expected
+                .to_not be NetboxClientRuby::Secrets.public_send(method, id)
+            end
+          end
+
+          context 'is an Entity object' do
+            it { is_expected.to respond_to(:get!) }
+          end
+        end
+      end
+
     end
   end
 end
