a8fd89ced8a6ff3b155bc6bbc60793dc556d416b
Christian Mäder <cma@nine.ch>
2017-11-30 10:48:12 +0100

    💄 Correctly indent spaces

--- spec/netbox_client_ruby/api/ipam_spec.rb
+++ spec/netbox_client_ruby/api/ipam_spec.rb
@@ -4,60 +4,55 @@ require 'spec_helper'
 module NetboxClientRuby
   module IPAM
     describe IPAM do
-    
       {
-        roles: Roles,
-        ip_addresses: IpAddresses,
-        vlan_groups: VlanGroups,
-        vrfs: Vrfs,
-        vlans: Vlans,
-        rirs: Rirs,
-        prefixes: Prefixes
+        cluster_types: ClusterTypes,
+        cluster_groups: ClusterGroups,
+        clusters: Clusters,
+        virtual_machines: VirtualMachines,
+        interfaces: Interfaces,
       }.each do |method, klass|
         describe ".#{method}" do
           subject { IPAM.public_send(method) }
-    
+
           context 'is of the correct type' do
             it { is_expected.to be_a klass }
           end
-    
+
           context 'is a different instance each time' do
             it do
               is_expected
                 .to_not be IPAM.public_send(method)
             end
           end
-    
+
           context 'is an Entities object' do
             it { is_expected.to respond_to(:get!) }
           end
         end
       end
-    
+
       {
-        role: Role,
-        ip_address: IpAddress,
-        vlan_group: VlanGroup,
-        vrf: Vrf,
-        vlan: Vlan,
-        rir: Rir,
-        prefix: Prefix
+        cluster_type: ClusterType,
+        cluster_group: ClusterGroup,
+        cluster: Cluster,
+        virtual_machine: VirtualMachine,
+        interface: Interface,
       }.each do |method, expected_class|
         describe ".#{method}" do
           let(:id) { 1 }
           subject { IPAM.public_send(method, id) }
-    
+
           context 'is of the expected type' do
             it { is_expected.to be_a expected_class }
           end
-    
+
           context 'it is a new instance each time' do
             it do
               is_expected
                 .to_not be IPAM.public_send(method, id)
             end
           end
-    
+
           context 'is an Entity object' do
             it { is_expected.to respond_to(:get!) }
           end
--- spec/netbox_client_ruby/api/tenancy_spec.rb
+++ spec/netbox_client_ruby/api/tenancy_spec.rb
@@ -10,18 +10,18 @@ module NetboxClientRuby
       }.each do |method, klass|
         describe ".#{method}" do
           subject { Tenancy.public_send(method) }
-    
+
           context 'is of the correct type' do
             it { is_expected.to be_a klass }
           end
-    
+
           context 'is a different instance each time' do
             it do
               is_expected
                 .to_not be Tenancy.public_send(method)
             end
           end
-    
+
           context 'is an Entities object' do
             it { is_expected.to respond_to(:get!) }
           end
