35a191b43c85cf7aacd01deb1a1981501e775ee7
Christian Mäder <cma@nine.ch>
2018-04-10 15:48:14 +0200

    🐞 Detects virtual machine interfaces
    Previously, when hoping from the IP address to the interface associated
    with that IP, NetboxClientRuby would always return a `DCIM::Interface`
    instance.
    
    ```ruby
    # would always return a DCIM::Interface
    NetboxClientRuby.ip_addresses.first.interface
    ```
    
    This is wrong, because if the IP address is assigned to a virtual
    machine, then we would expected to receive a
    `Virtualization::Interface` instead.
    
    This commit fixes #24.

--- lib/netbox_client_ruby/api/ipam/ip_address.rb
+++ lib/netbox_client_ruby/api/ipam/ip_address.rb
@@ -1,4 +1,5 @@
 require 'netbox_client_ruby/entity'
+require 'netbox_client_ruby/error/local_error'
 require 'netbox_client_ruby/api/dcim/interface'
 require 'netbox_client_ruby/api/ipam/vrf'
 require 'netbox_client_ruby/api/tenancy/tenant'
@@ -24,7 +25,6 @@ module NetboxClientRuby
         vrf: proc { |raw_data| Vrf.new raw_data['id'] },
         tenant: proc { |raw_data| Tenancy::Tenant.new raw_data['id'] },
         status: proc { |raw_data| STATUS_VALUES.key(raw_data['value']) || raw_data['value'] },
-        interface: proc { |raw_data| DCIM::Interface.new raw_data['id'] },
         address: proc { |raw_ip| IPAddress.parse(raw_ip) }
       )
       readonly_fields :display_name
@@ -32,6 +32,18 @@ module NetboxClientRuby
       def status=(value)
         status_code_lookup = STATUS_VALUES.fetch(value, value)
         method_missing(:status=, status_code_lookup)
+      end
+
+      def interface
+        interface_data = data['interface']
+
+        return nil unless interface_data
+
+        if interface_data.key? ('virtual_machine')
+          Virtualization::Interface.new interface_data['id']
+        else interface_data.key? ('device')
+          DCIM::Interface.new interface_data['id']
+        end
       end
     end
   end
