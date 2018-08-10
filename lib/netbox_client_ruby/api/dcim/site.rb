a445d49e0b8f1c57bd6458fe25900b6911c5b716
Christian Mäder <cma@nine.ch>
2018-08-10 15:51:45 -0700

    ✨ Added relations to DCIM::Site

--- lib/netbox_client_ruby/api/dcim/site.rb
+++ lib/netbox_client_ruby/api/dcim/site.rb
@@ -1,17 +1,30 @@
 require 'netbox_client_ruby/entity'
+require 'netbox_client_ruby/api/dcim/region'
 
 module NetboxClientRuby
   module DCIM
     class Site
       include Entity
 
+      STATUS_VALUES = {
+        active: 1,
+        planned: 2,
+        retired: 4
+      }.freeze
+
       id id: :id
-      readonly_fields :count_prefixes, :count_vlans, :count_racks, :count_devices,
-                      :count_circuits
+      readonly_fields :count_prefixes, :count_vlans, :count_racks,
+                      :count_devices, :count_circuits
 
       deletable true
       path 'dcim/sites/:id.json'
       creation_path 'dcim/sites/'
+      object_fields(
+        region: proc { |raw_region| DCIM::Region.new raw_region['id'] },
+        status: proc do |raw_status|
+          STATUS_VALUES.key(raw_status['value']) || raw_status['value']
+        end
+      )
     end
   end
 end
