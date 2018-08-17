99e0e2f2d2b5ccbfd54985fc709b4a21a2d2c00c
Christian Mäder <cma@nine.ch>
2018-08-17 13:49:18 -0700

    🔍 Improve Circuit Specs

--- spec/netbox_client_ruby/api/circuits_spec.rb
+++ spec/netbox_client_ruby/api/circuits_spec.rb
@@ -28,6 +28,33 @@ module NetboxClientRuby
           end
         end
       end
+
+      {
+        provider: Provider,
+        circuit: Circuit,
+        circuit_termination: CircuitTermination,
+        circuit_type: CircuitType
+      }.each do |method, expected_class|
+        describe ".#{method}" do
+          let(:id) { 1 }
+          subject { described_class.public_send(method, id) }
+
+          context 'is of the expected type' do
+            it { is_expected.to be_a expected_class }
+          end
+
+          context 'it is a new instance each time' do
+            it do
+              is_expected
+                .to_not be described_class.public_send(method, id)
+            end
+          end
+
+          context 'is an Entity object' do
+            it { is_expected.to respond_to(:get!) }
+          end
+        end
+      end
     end
   end
 end
