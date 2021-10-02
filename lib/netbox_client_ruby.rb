9467ef4e4d90539ee6874c0750d40073e39b5807
Andre Meij <andre@itsmeij.com>
2021-10-02 16:57:42 +0200

    Use new dry-configurable api 'default:'

--- lib/netbox_client_ruby.rb
+++ lib/netbox_client_ruby.rb
@@ -13,19 +13,19 @@ module NetboxClientRuby
       setting :rsa_private_key do
         # the default is intentionally not `~/.ssh/id_rsa`,
         # to not accidentally leak someone's main rsa private key
-        setting :path, '~/.ssh/netbox_rsa'
+        setting :path, default: '~/.ssh/netbox_rsa'
         setting :password
       end
     end
     setting :pagination do
-      setting :default_limit, 50
-      setting :max_limit, MAX_SIGNED_64BIT_INT
+      setting :default_limit, default: 50
+      setting :max_limit, default: MAX_SIGNED_64BIT_INT
     end
   end
 
   setting :faraday do
-    setting :adapter, :net_http
+    setting :adapter, default: :net_http
     setting :logger
-    setting :request_options, open_timeout: 1, timeout: 5
+    setting :request_options, default: { open_timeout: 1, timeout: 5 }
   end
 end
