072a58ad25ba2d47662beab708a0d32ed17e52e9
Bastian Triller <bastian.triller@gmail.com>
2021-11-26 13:28:10 +0100

    Add config contexts

--- lib/netbox_client_ruby/api/extras.rb
+++ lib/netbox_client_ruby/api/extras.rb
@@ -1,3 +1,5 @@
+require 'netbox_client_ruby/api/extras/config_context'
+require 'netbox_client_ruby/api/extras/config_contexts'
 require 'netbox_client_ruby/api/extras/journal_entry'
 require 'netbox_client_ruby/api/extras/journal_entries'
 require 'netbox_client_ruby/api/extras/tag'
@@ -6,6 +8,7 @@ require 'netbox_client_ruby/api/extras/tags'
 module NetboxClientRuby
   module Extras
     {
+      config_contexts: ConfigContexts,
       journal_entries: JournalEntries,
       tags: Tags
     }.each_pair do |method_name, class_name|
@@ -14,6 +17,7 @@ module NetboxClientRuby
     end
 
     {
+      config_context: ConfigContext,
       journal_entry: JournalEntry,
       tag: Tag
     }.each_pair do |method_name, class_name|
---
+++ lib/netbox_client_ruby/api/extras/config_context.rb
@@ -0,0 +1,25 @@
+require 'netbox_client_ruby/entity'
+
+module NetboxClientRuby
+  module Extras
+    class ConfigContext
+      include Entity
+
+      id id: :id
+      deletable true
+      path 'extras/config-contexts/:id.json'
+      creation_path 'extras/config-contexts/'
+      object_fields(
+        regions: proc { |raw_data| DCIM::Region.new raw_data['id'] },
+        sites: proc { |raw_data| DCIM::Site.new raw_data['id'] },
+        roles: proc { |raw_data| DCIM::DeviceRole.new raw_data['id'] },
+        platforms: proc { |raw_data| DCIM::Platform.new raw_data['id'] },
+        cluster_groups: proc { |raw_data| Virtualization::ClusterGroup.new raw_data['id'] },
+        clusters: proc { |raw_data| Virtualization::Cluster.new raw_data['id'] },
+        tenant_groups: proc { |raw_data| Tenancy::TenantGroup.new raw_data['id'] },
+        tenants: proc { |raw_data| Tenancy::Tenant.new raw_data['id'] },
+        tags: proc { |raw_data| Tag.new raw_data['id'] }
+      )
+    end
+  end
+end
