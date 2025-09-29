235389903772814129fd5a7613b83e8e4e0e0569
Nicolas Rodriguez <nico@nicoladmin.fr>
2025-09-30 00:22:05 +0200

    Improve Rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
-require 'bundler/setup'
+require 'warning'
+
+# Ignore all warnings in Gem dependencies
+Gem.path.each do |path|
+  Warning.ignore(//, path)
+end
+
 require 'netbox-client-ruby'
 require 'shared_context'
 require 'faraday/net_http_persistent' if Faraday::VERSION > '2'
@@ -8,12 +14,16 @@ require 'faraday/net_http_persistent' if Faraday::VERSION > '2'
 RSpec.configure do |config|
   config.example_status_persistence_file_path = '.rspec_status'
 
+  config.color = true
+  config.fail_fast = false
+  config.fail_if_no_examples = true
+
+  config.order = :random
+  Kernel.srand config.seed
+
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  config.order = 'random'
   config.include_context 'connection setup'
-
-  config.fail_if_no_examples = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
