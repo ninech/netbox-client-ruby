9faedeee298e1fb99a605e1a2bf3726dcacc92dc
Letam Patrick-Bienwi <letam.patrick-bienwi@york.ac.uk>
2026-01-13 16:37:35 +0000

    Adjusted files for rubocop

--- lib/netbox_client_ruby/api/dcim/locations.rb
+++ lib/netbox_client_ruby/api/dcim/locations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module NetboxClientRuby
   module DCIM
     class Locations
--- spec/netbox_client_ruby/api/dcim/locations_spec.rb
+++ spec/netbox_client_ruby/api/dcim/locations_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe NetboxClientRuby::DCIM::Locations, faraday_stub: true do
